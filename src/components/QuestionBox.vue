6ff82abdeac25c14b275f08dfcc60ec19538021b
mdjuniooor <mmdsds@u.sogang.ac.kr>
2020-04-12 20:39:40 +0900

    Setting and Apply Quiz API

---
+++ src/components/QuestionBox.vue
@@ -0,0 +1,19 @@
+<template>
+   <div class="question-box-container">
+      <b-jumbotron>
+
+        <template v-slot:lead>
+          Some Question Here?
+        </template>
+
+        <hr class="my-4">
+
+        <p>
+          List of answers
+        </p>
+
+        <b-button variant="primary" href="#">Do Something</b-button>
+        <b-button variant="success" href="#">Do Something Else</b-button>
+      </b-jumbotron>
+  </div>
+</template>
