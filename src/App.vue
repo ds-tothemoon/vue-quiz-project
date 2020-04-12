84661a1b3edb1de821fb712f604a41db6ddf510d
mdjuniooor <mmdsds@u.sogang.ac.kr>
2020-04-12 21:41:40 +0900

    Add answer selection && shuffling && counting

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,19 @@
 <template>
   <div id="app">
-    <Header/>
+    <Header
+      :numCorrect="numCorrect"
+      :numTotal="numTotal"
+    />
     <b-container class="bv-example-row">
       <b-row>
-        <b-col sm="6" offset="3"><QuestionBox/></b-col>
+        <b-col sm="6" offset="3">
+          <QuestionBox
+            v-if="questions.length"
+            :currentQuestion="questions[index]"
+            :next="next"
+            :increment="increment"
+          />
+        </b-col>
       </b-row>
     </b-container>
   </div> 
@@ -21,7 +31,21 @@ export default {
   },
   data() {
     return {
-      questions: []
+      questions: [],
+      index: 0,
+      numCorrect: 0,
+      numTotal: 0
+    }
+  },
+  methods: {
+    next() {
+      this.index++
+    },
+    increment(isCorrect){
+      if(isCorrect){
+        this.numCorrect++
+      }
+      this.numTotal++
     }
   },
   mounted: function(){
--- src/components/QuestionBox.vue
+++ src/components/QuestionBox.vue
@@ -3,17 +3,110 @@
       <b-jumbotron>
 
         <template v-slot:lead>
-          Some Question Here?
+          {{ currentQuestion.question }}
         </template>
 
         <hr class="my-4">
+        <b-list-group>
+          <b-list-group-item 
+            v-for="(answer,index) in answers" 
+            :key="index"
+            @click.prevent="selectAnswer(index)"
+            :class="[selectedIndex === index ? 'selected' : '']"
+            >
+            {{ answer }}
+           </b-list-group-item>
+        </b-list-group>
 
-        <p>
-          List of answers
-        </p>
-
-        <b-button variant="primary" href="#">Do Something</b-button>
-        <b-button variant="success" href="#">Do Something Else</b-button>
+        <b-button 
+          variant="primary" 
+          @click="submitAnswer"
+          :disabled="selectedIndex === null"
+        >
+          Submit
+        </b-button>
+        <b-button @click="next" variant="success">next</b-button>
       </b-jumbotron>
   </div>
 </template>
+
+<script>
+import _ from 'lodash'
+export default {
+  props: {
+    currentQuestion: Object,
+    next: Function,
+    increment: Function
+  },
+  data(){
+    return {
+      selectedIndex: null,
+      correctIndex: null,
+      shuffledAnswers: []
+    }
+  }
+  ,
+  methods:{
+    selectAnswer(index){
+      this.selectedIndex = index
+    },
+    shuffleAnswers(){
+      let answers = [...this.currentQuestion.incorrect_answers, this.currentQuestion.correct_answer]
+      this.shuffledAnswers = _.shuffle(answers)
+      this.correctIndex = this.shuffledAnswers.indexOf(this.currentQuestion.correct_answer)
+    },
+    submitAnswer(){
+      let isCorrect = false
+
+      if (this.selectedIndex === this.correctIndex){
+        isCorrect = true
+      }
+
+      this.increment(isCorrect)
+    }
+  },  
+  computed: {
+    answers(){
+      let answers = [...this.currentQuestion.incorrect_answers]
+      answers.push(this.currentQuestion.correct_answer)      
+      return answers
+    }
+  },
+  watch:{
+    currentQuestion:{
+      immediate: true,
+      handler() {
+        this.selectedIndex = null
+        this.shuffleAnswers()
+      }
+    }
+  },
+}
+</script>
+
+<style scope>
+.list-group {
+  margin-bottom: 15px
+}
+
+.list-group-item:hover {
+  background: #EEE;
+  cursor: pointer;
+}
+
+.btn {
+  margin: 5px
+}
+
+.selected {
+  background-color: skyblue;
+}
+
+.correct {
+  background-color: green;
+}
+
+.incorrect {
+  background-color: red;
+}
+</style>
